3303e67689f1bf681ea638b889b427c656e235b0
OJ Raqueño <oj.raqueno@gmail.com>
2017-07-21 20:01:25 +0800

    add simple injector

---
+++ MVC5_R/MVC5_R/App_Start/DependencyConfig.cs
@@ -0,0 +1,24 @@
+﻿using MVC5_R.Data;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+using System.Web.Mvc;
+
+namespace MVC5_R
+{
+    public class DependencyConfig
+    {
+        public static Container Container { get; private set; }
+
+        public static void Configure()
+        {
+            Container = new Container();
+            
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(Container));
+
+            Container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+
+            Container.Register<ApplicationDbContext, ApplicationDbContext>(Lifestyle.Scoped);
+        }
+    }
+}
