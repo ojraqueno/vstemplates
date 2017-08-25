0aa0201e75249ac4100840db17d7ff9c95ad47db
OJ Raqueño <oj.raqueno@gmail.com>
2017-08-25 15:26:20 +0800

    user mediator for forgot password implementation

--- MVC5_R/MVC5_R/Controllers/AccountController.cs
+++ MVC5_R/MVC5_R/Controllers/AccountController.cs
@@ -141,45 +141,28 @@ namespace MVC5_R.Controllers
 
             return View(nameof(ConfirmEmail));
         }
-
-        //
-        // GET: /Account/ForgotPassword
+        
         [AllowAnonymous]
         public ActionResult ForgotPassword()
         {
             return View();
         }
-
-        //
-        // POST: /Account/ForgotPassword
+        
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        public async Task<ActionResult> ForgotPassword(ForgotPassword.Command command)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                var user = await _userManager.FindByNameAsync(model.Email);
-                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user.Id)))
-                {
-                    // Don't reveal that the user does not exist or is not confirmed
-                    return View("ForgotPasswordConfirmation");
-                }
-
-                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                // Send an email with this link
-                // string code = await _userManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                // await _userManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                return View(command);
             }
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
+            await _mediator.Send(command);
 
-        //
-        // GET: /Account/ForgotPasswordConfirmation
+            return RedirectToAction(nameof(ForgotPasswordConfirmation));
+        }
+        
         [AllowAnonymous]
         public ActionResult ForgotPasswordConfirmation()
         {
---
+++ MVC5_R/MVC5_R/Features/Account/ForgotPassword.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation;
+using MediatR;
+using MVC5_R.Controllers;
+using MVC5_R.Infrastructure.Identity;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_R.Features.Account
+{
+    public class ForgotPassword
+    {
+        public class Command : IRequest
+        {
+            public string Email { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.Email)
+                    .NotEmpty();
+
+                RuleFor(c => c.Email)
+                    .EmailAddress();
+            }
+        }
+
+        public class Handler : IAsyncRequestHandler<Command>
+        {
+            private readonly ApplicationUserManager _userManager;
+
+            public Handler(ApplicationUserManager applicationUserManager)
+            {
+                _userManager = applicationUserManager;
+            }
+
+            public async Task Handle(Command model)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user.Id)))
+                {
+                    // Do nothing: Don't reveal that the user does not exist or is not confirmed
+                }
+                else
+                {
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    var urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+                    string code = await _userManager.GeneratePasswordResetTokenAsync(user.Id);
+                    var callbackUrl = urlHelper.Action(nameof(AccountController.ResetPassword), "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Current.Request.Url.Scheme);
+
+                    // No await means fire and forget
+                    _userManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>. If you did not request for a password reset, no action is required.");
+                }
+            }
+        }
+    }
+}
