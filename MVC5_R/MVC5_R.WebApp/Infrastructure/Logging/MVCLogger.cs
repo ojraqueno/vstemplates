e80d18bbd090791e75c081252e479b088ab3db51
OJ Raqueño <oj.raqueno@gmail.com>
2017-11-17 12:33:54 +0800

    add logged fields

--- MVC5_R/MVC5_R.WebApp/Infrastructure/Logging/MVCLogger.cs
+++ MVC5_R/MVC5_R.WebApp/Infrastructure/Logging/MVCLogger.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using MVC5_R.Infrastructure.Data;
+using MVC5_R.Infrastructure.Logging;
 using MVC5_R.Models;
 using System;
 using System.Web.Mvc;
@@ -15,16 +16,22 @@ namespace MVC5_R.WebApp.Infrastructure.Logging
                 Action = Convert.ToString(filterContext.RouteData.Values["action"]),
                 Controller = Convert.ToString(filterContext.RouteData.Values["controller"]),
                 LoggedOn = DateTime.UtcNow,
+                Level = LogLevel.Error,
                 Message = filterContext.Exception.Message,
+                Request = GetRequest(filterContext),
                 StackTrace = filterContext.Exception.StackTrace,
                 UserId = filterContext.HttpContext.User.Identity.GetUserId()
             };
 
-            using (var db = new ApplicationDbContext())
-            {
-                db.LogEntries.Add(logEntry);
-                db.SaveChanges();
-            }
+            Logger.Log(logEntry);
+        }
+
+        private static string GetRequest(ExceptionContext filterContext)
+        {
+            var headers = filterContext.HttpContext.Request.ServerVariables["ALL_RAW"].Replace("\r\n", Environment.NewLine);
+            var form = filterContext.HttpContext.Request.Form.ToString();
+
+            return headers + Environment.NewLine + form;
         }
     }
 }
