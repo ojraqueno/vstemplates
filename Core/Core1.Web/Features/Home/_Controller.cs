c65a830f449acf9b0cf0ba175d9ae5826cb2268e
OJ Raqueño <oj.raqueno@gmail.com>
2018-07-11 16:06:03 +0800

    add styles to home pages

--- Core/Core1.Web/Features/Home/_Controller.cs
+++ Core/Core1.Web/Features/Home/_Controller.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Core1.Web.Features.Shared;
+﻿using Core1.Web.Features.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Core1.Features.Home
 {
@@ -19,15 +15,11 @@ namespace Core1.Features.Home
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
