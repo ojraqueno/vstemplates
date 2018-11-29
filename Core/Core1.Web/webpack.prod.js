5e5f52ac7b815eed5186770f73030826a4a5402e
OJ Raqueño <oj.raqueno@gmail.com>
2018-11-29 14:17:46 +0800

    update nuget / npm packages

---
+++ Core/Core1.Web/webpack.prod.js
@@ -0,0 +1,101 @@
+﻿let path = require('path');
+var webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        'accounts-forgot-password': './wwwroot/js/accounts/forgot-password.js',
+        'accounts-index': './wwwroot/js/accounts/index.js',
+        'accounts-login': './wwwroot/js/accounts/login.js',
+        'accounts-register': './wwwroot/js/accounts/register.js',
+        'accounts-reset-password': './wwwroot/js/accounts/reset-password.js',
+        'admin-index': './wwwroot/js/admin/index.js',
+        'emails-index': './wwwroot/js/emails/index.js',
+        'filters': './wwwroot/js/shared/filters.js',
+        'logs-index': './wwwroot/js/logs/index.js',
+        'shared-layout': './wwwroot/js/shared/layout.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            // the url-loader uses DataUrls.
+            // the file-loader emits files.
+            {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                // Limiting the size of the woff fonts breaks font-awesome ONLY for the extract text plugin
+                // loader: "url?limit=10000"
+                use: "url-loader"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    output: {
+        path: path.resolve('./wwwroot/dist'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+        //new BundleAnalyzerPlugin()
+    ],
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
+    }
+};
