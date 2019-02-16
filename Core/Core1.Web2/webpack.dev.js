8001347b261fd5b43c791f6d7f6b0198a6648e7f
OJ Raqueño <oj.raqueno@gmail.com>
2019-02-16 11:54:33 +0800

    remove missing bundles.

--- Core/Core1.Web2/webpack.dev.js
+++ Core/Core1.Web2/webpack.dev.js
@@ -7,14 +7,10 @@ module.exports = {
     mode: 'development',
     entry: {
         'accounts-forgot-password': './wwwroot/js/accounts/forgot-password.js',
-        'accounts-index': './wwwroot/js/accounts/index.js',
         'accounts-login': './wwwroot/js/accounts/login.js',
         'accounts-register': './wwwroot/js/accounts/register.js',
         'accounts-reset-password': './wwwroot/js/accounts/reset-password.js',
-        'admin-index': './wwwroot/js/admin/index.js',
-        'emails-index': './wwwroot/js/emails/index.js',
         'filters': './wwwroot/js/shared/filters.js',
-        'logs-index': './wwwroot/js/logs/index.js',
         'shared-layout': './wwwroot/js/shared/layout.js'
     },
     module: {
