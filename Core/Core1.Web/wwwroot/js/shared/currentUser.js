f28e95ed196d5df534695744ec339bfc4a0a15b4
OJ Raqueño <oj.raqueno@gmail.com>
2019-04-13 02:05:01 +0800

    add current user

---
+++ Core/Core1.Web/wwwroot/js/shared/currentUser.js
@@ -0,0 +1,7 @@
+﻿let currentUser = undefined;
+
+if (document.getElementById('CurrentUser')) {
+    currentUser = JSON.parse(document.getElementById('CurrentUser').innerHTML);
+}
+
+export default currentUser;
